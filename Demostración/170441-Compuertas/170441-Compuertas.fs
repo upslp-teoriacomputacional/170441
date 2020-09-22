be69beac0185d599f76bab810589ca7fc7228786
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-09-21 21:01:55 -0500

    Tarea_Demostración

---
+++ Demostración/170441-Compuertas/170441-Compuertas.fs
@@ -0,0 +1,55 @@
+﻿(*
+ * *****************************************************************************
+ *        Name:    Juan Gerardo     
+ *     Surname:    Montalvo Becerra
+ *          ID:    170441
+ *       Major:    IT Engineering
+ * Institution:    Universidad Politécnica de San Luis Potosí
+ *   Professor:    Juan Carlos González Ibarra
+ * Description:    Made in VSC, using F# language.
+ *                
+ *  Written:       21/09/2020
+ *  Last updated:  21/09/2020
+ **************************************************************************** *)
+open System
+
+[<EntryPoint>]
+let main argv = 
+    //  We start declaring the list that includes false and true values
+    let booleans = [false; true]
+    // The first table executes the OR operation between x and y
+    printfn "x\ty\tx or y"
+    printfn "----------------------"
+    //  The chained FOR creates all the possible sets between x and y
+    for x in booleans do
+        for y in booleans do
+            // To obtain a logic OR you could use "or" or "||"
+            printfn" %A\t%A\t%A" x y (x or y)
+    printf "\n\n"
+    // The second table executes the AND operation between x and y
+    printfn "x\ty\tx and y"
+    printfn "----------------------"
+    //  The chained FOR creates all the possible sets between x and y
+    for x in booleans do
+        for y in booleans do
+            // In this case, you can only use &&.
+            printfn" %A\t%A\t%A" x y (x && y)
+    printf "\n\n"
+    // The Third table executes a not operation of only the x value
+    printfn "x\tnot x"
+    printfn "-------------"
+    //  The FOR is used to search for every element of the list called booleans
+    for x in booleans do
+        //  To create a not operation, you simply use "not" before the variable  
+        printfn" %A\t%A" x (not x)
+    printf "\n\n"
+    // Finally, we have the last table that creates an Inclusive OR AKA XOR
+    printfn "x\ty\tx ^ y"
+    printfn "----------------------"
+    //  The FOR is used to search for every element of the list called booleans
+    for x in booleans do
+        for y in booleans do
+            //  This is more complex that the other logic operations. This time, It is necessary that we create
+            //  the logic behind the XOR which is A ^ B = (A OR B) AND ~(A OR B)
+            printfn" %A\t%A\t%A" x y ((x || y) && not (x && y)) 
+    0 // return an integer exit code
---
+++ Demostración/170441-Compuertas/README.md
@@ -0,0 +1,9 @@
+# **How did you solve the problem?**
+First, for me to understand the problem and what I had to do, I started by executing the Python code in repl.it. Once I knew the basics on one side, then I searched for similar commands that would work with my program in F#.
+At the same time, I was looking for commands, I went ahead with the coding and included all that I was looking for. As a result of this, I ended up with some problems such as wrong or false declarations that did not even exist in the F# language. Despite of random errors, all the rest of it was easy and not time consuming.
+# **Commands for executing operations**
+-	not	Boolean negation
+-	||	Boolean OR
+-	&&	Boolean AND
+# **Problems and solutions during the work’s programming**
+My only issue with during the programming was the inclusive or. At first, I did not remember what that was, but with a little of investigation I ended up knowing that was equal to a XOR operation. F# do not accept such operation, hence I used the decomposed formula which uses OR, NOT and AND logic.
