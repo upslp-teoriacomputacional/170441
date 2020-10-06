8f48ea31194dfddac3cbfe055b347f2337e12b86
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-10-05 22:13:16 -0500

    tarea_afd

---
+++ AFD/AFD_170441/AFD_170441.fs
@@ -0,0 +1,92 @@
+﻿(*
+ * *****************************************************************************
+ *        Name:    Juan Gerardo     
+ *     Surname:    Montalvo Becerra
+ *          ID:    170441
+ *       Major:    IT Engineering
+ * Institution:    Universidad Politécnica de San Luis Potosí
+ *   Professor:    Juan Carlos González Ibarra
+ * Description:    Made in VSC, using F# language.
+ *                
+ *  Written:       04/10/2020
+ *  Last updated:  05/10/2020
+ **************************************************************************** *)
+open System
+//  Initial variables
+let mutable simbolo = ""
+let fin = None
+//  Function that matches a given char with a simbol, digit, or null
+let caracter (character:char) :int =
+    simbolo <- ""
+    
+    match character with 
+    | '0'| '1'| '2'| '3'| '4'| '5'| '6'| '7'| '8'| '9' -> simbolo <- " Digito " ;0
+    | '+'| '-'| '*'| '/'  -> simbolo <- "Operador";1
+    | ' ' -> 2
+    | _ -> 3 
+//  Function of Body
+let body () = 
+    printfn "+--------------+---------+-----------+---------------+"
+// Funtion that prints a title
+let encabezado () =
+    printfn "|  Edo. Actual |Caracter |  Simbolo  |Edo. Siguiente |"
+    body()
+//  Function that prints a set of values
+let contenido estadosig character simbolo estado =
+    if estado = 4 then
+        printfn "|     %A        |  %A    |%A |      ERR      |" estadosig character simbolo 
+    else 
+        printfn "|     %A        |  %A    |%A |       %A       |" estadosig character simbolo estado
+
+    
+
+
+[<EntryPoint>]
+//  Main code
+let main argv =
+    //  E = 4 | A = 5
+    //  Table of states
+    let tabla = [ [1;4;4];[4;2;4];[3;4;4];[4;4;5] ]
+    //  Variable of initial state 
+    let mutable estado = 0
+    printfn "+-------------------------------------+"
+    printfn "|    Ingrese una cadena a evaluar:    |"
+    printfn "+-------------------------------------+"
+    //  Input of a string
+    let cadena = Console.ReadLine()
+    body()
+    encabezado()
+    //  Cicle FOR that iterates the input string
+    for character in cadena do
+        //  Asignation of state to the next state
+        let estadosig = estado
+        //  Call the caracter function that returns a specific value
+        let charcaracter = caracter character
+        //  Determine if the value was not invalid
+        if (charcaracter = 3) then
+            printfn "< < < C A R A C T E R   I N V A L I D O > > >"
+            exit 0
+        //  Change the value of the state with a value of the states' table
+        estado <- tabla.[estado].[charcaracter]
+        // If the state is 4 then is a invalid string
+        if (estado = 4) then
+            contenido estadosig character simbolo estado
+            body()
+            printfn "|              Cadena No Valida :(                   |"
+            printfn "+----------------------------------------------------+"
+            exit 0
+        //  Otherwise it prints the information
+        contenido estadosig character simbolo estado
+        body()
+
+    //  At the end, if the state is different than 3, it is invalid
+    if (estado <> 3) then
+        printfn "|              Cadena No Valida :(                   |"
+        printfn "+----------------------------------------------------+"
+    //  And if it is equal then it is valid
+    if (estado = 3) then
+        printfn "|     %A        |         |Fin Cadena |               |" estado
+        body()
+        printfn "|                Cadena Valida                       |"
+        printfn "+----------------------------------------------------+"    
+    0 // return an integer exit code
---
+++ AFD/AFD_170441/README.md
@@ -0,0 +1,12 @@
+# **How did you solve the problem?**
+First, for me to understand the problem and what I had to do, I started by executing the Python code in repl.it. Once I knew the basics on one side, then I searched for similar commands that would work with my program in F#.
+At the same time, I was looking for commands, I went ahead with the coding and included all that I was looking for. As a result of this, I ended up with some problems such as wrong or false declarations that did not even exist in the F# language. Despite of random errors, all the rest of it was easy and not time consuming.
+# **Commands for executing operations**
+-	mutable	The mutable keyword allows you to declare and assign values in a mutable variable.
+-	match		Pattern matching is used for control flow.
+-	Console.ReadLine()	Reads the next line of characters from the standard input stream.
+-	<>		Returns true if the left side is not equal to the right side; otherwise, returns false.	
+# **Problems and solutions during the work’s programming**
+This time, the main issue was the programming language itself. F# provides a good structure to build a comparison for the character, but in other aspects it will give you a hard time trying to find something more specific. For example, one of my problems was variables because I did no know that you could use mutable in order to alter the value assigned without having to declare it a second time.
+# **Is it a NP or P complexity?**
+Because the code includes a search, it indicates O(n²) for the complexity. Hence, the complexity will be polynomial.
