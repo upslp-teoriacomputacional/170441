6656fc60813be559d117c5616bfa6491dcec29ef
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-10-16 13:14:02 -0500

    NFA_v2

---
+++ AFN/AFN_170441/AFN_170441.fs
@@ -0,0 +1,183 @@
+﻿(*
+ * *****************************************************************************
+ *         Name:    Juan Gerardo     
+ *      Surname:    Montalvo Becerra
+ * Collaborator:    Vazquez Reyes Rodolfo Emanuel
+ *           ID:    170441
+ *        Major:    IT Engineering
+ *  Institution:    Universidad Politécnica de San Luis Potosí
+ *    Professor:    Juan Carlos González Ibarra
+ *  Description:    Made in VSC, using F# language.
+ *
+ *              
+ *      Written:    15/10/2020
+ * Last updated:    16/10/2020
+ **************************************************************************** *)
+open System
+//  Because we have nodes with two states, we need a second table that has those second values
+let tabla = [ [2;-1;-1]; [-10;-1;-1]; [3;-1;-1]; [8;-1;-1]; [-1;-1;5]; [6;-1;-1]; [7;-1;-1]; [1;-1;-1]; [-1;9;-1]; [3;-1;-1]; [-1;11;-1]; [7;-1;-1] ]
+let tabla2 = [ [-1;-1;-1]; [-1;-1;-1]; [8;-1;-1]; [4;-1;-1]; [-1;-1;-1]; [-1;-1;-1]; [10;-1;-1]; [10;-1;-1]; [-1;-1;-1]; [-1;-1;-1]; [-1;-1;-1]; [-1;-1;-1] ]
+//  Matching functions aba (Function Made by Vazquez Reyes Rodolfo Emanuel)
+let caracter (mtch:string) :string =
+    //  match evaluates the first char of the string and returns the same value
+    match mtch with 
+        | head when head.StartsWith("a") -> "a"
+        | head when head.StartsWith("b") -> "b"
+        | head when head.StartsWith("") -> "''"
+        | _ -> exit(0)
+//  Just a title with color (Function Made by Vazquez Reyes Rodolfo Emanuel)
+let title () =
+    Console.ForegroundColor<-ConsoleColor.Red
+    printfn "+-------------------------------------+"
+    printf "|    "
+    Console.ForegroundColor<-ConsoleColor.Green
+    printf "Ingrese una cadena a evaluar:"
+    Console.ForegroundColor<-ConsoleColor.Red
+    printfn "    |"
+    printfn "+-------------------------------------+"
+    Console.ForegroundColor<-ConsoleColor.White
+//  Prints a visible separation in the exit (Function Made by Vazquez Reyes Rodolfo Emanuel)
+let body () = 
+    for x in [0 .. 30] do
+        if x % 2 = 0 then
+            Console.ForegroundColor<-ConsoleColor.Green
+            printf "+-----"
+        else
+            Console.ForegroundColor<-ConsoleColor.Red
+            printf "+-----"
+    printfn ""
+    Console.ForegroundColor<-ConsoleColor.White
+//  Prints a transition table for the evaluated string (Function Made by Vazquez Reyes Rodolfo Emanuel & Montalvo Becerra Juan Gerardo)
+let transitionTable (cd:string)(accepted:bool) =
+    let mutable key = cd
+    let mutable color = ConsoleColor.White
+    let mutable temp = ""
+    //  The color is defined depending on the variable accepted
+    if accepted then
+        color <- ConsoleColor.Green
+    else
+        color <- ConsoleColor.Red
+    Console.ForegroundColor<-color
+    printfn "+---------------+---------------+---------------+---------------+"
+    printfn "|  Edo. Actual  |    Caracter   |    Simbolo    |Edo. Siguiente |"
+    while not (key.Equals("")) do
+        try
+            temp <- key.Remove (key.IndexOf(")")+1)
+        with
+          | :? System.ArgumentOutOfRangeException -> temp <- key
+        try
+            key <- key.Remove (key.IndexOf("("),key.IndexOf(")")+2)
+        with
+          | :? System.ArgumentOutOfRangeException -> key <- key.Remove (key.IndexOf("("),key.IndexOf(")")+1)
+        //  temp|>printfn "%s"
+        //  key|>printfn "%s"
+        Console.ForegroundColor<-color
+        printfn "+---------------+---------------+---------------+---------------+"
+        printf "|" 
+        Console.ForegroundColor<-ConsoleColor.White
+        printf "\t%s\t" (temp.Substring(1,temp.IndexOf(",")-1)) 
+        Console.ForegroundColor<-color
+        printf "|"
+        Console.ForegroundColor<-ConsoleColor.White 
+        printf "\t%s\t"  (temp.Substring(temp.IndexOf(" ")+1,temp.IndexOf(":")-temp.IndexOf(" ")-1)) 
+        Console.ForegroundColor<-color
+        printf "|" 
+        Console.ForegroundColor<-ConsoleColor.White
+        printf "\t%s\t" (temp.Substring(temp.IndexOf(":")+1,temp.IndexOf("-")-temp.IndexOf(":")-1)) 
+        Console.ForegroundColor<-color
+        printf "|" 
+        Console.ForegroundColor<-ConsoleColor.White
+        printf "\t%s\t"  (temp.Substring(temp.IndexOf(">")+1,temp.IndexOf(")")-temp.IndexOf(">")-1))
+        Console.ForegroundColor<-color
+        printfn "|" 
+    //  At the end, it displays if the chain was valid or not
+    if accepted then
+        printfn "+---------------+---------------+---------------+---------------+"
+        printfn "|                       Cadena Valida                           |"
+        printfn "+---------------------------------------------------------------+"
+    else
+        printfn "+---------------+---------------+---------------+---------------+"
+        printfn "|                     Cadena No Valida :(                       |"
+        printfn "+---------------------------------------------------------------+"
+    Console.ForegroundColor<-ConsoleColor.White
+//  Recursive function that display every posible way for the STRING in the NFA 
+//  (Function Made by Vazquez Reyes Rodolfo Emanuel & Montalvo Becerra Juan Gerardo)
+let rec node (nodo:int) (cad:string) (state:string) =
+    //  Variables that recover the current state and send the next
+    let mutable st = ""
+    let mutable proof = ""
+    //  Because the very first node is "" we call a try catch that ignores the code in the first one
+    try
+        proof <- state.Remove(0,state.Length-2)
+        proof <- proof.Remove 1
+    with
+      | :? System.ArgumentOutOfRangeException -> printf ""
+    //  At the start, it checks if it already acceptable
+    if (proof.Equals("A")) then
+        body ()
+        printfn "%A" state
+        Console.ForegroundColor<-ConsoleColor.Green
+        printfn " = It reaches the final state"
+        Console.ForegroundColor<-ConsoleColor.White
+        transitionTable state true
+    //  It even checks if it's not acceptable
+    elif (proof.Equals("E")) then
+        body ()
+        printfn "%A" state
+        Console.ForegroundColor<-ConsoleColor.Red
+        printfn " = It does not reach the final state"
+        Console.ForegroundColor<-ConsoleColor.White
+        transitionTable state false
+    //  If neither of those occurred, it continues until it occurs    
+    else
+        let mutable chain = ""
+        //  Uses the function match on the cad string
+        let result = caracter cad
+        //  When the node is on q8 or q10
+        if (tabla.[nodo].[1] <> -1) then
+            //  If it is equal to a then it continues to the nex node
+            if result.Equals("a") then
+                st <- state + "(q" + string nodo + ", " + result + ":a" + "->" + "q" + string tabla.[nodo].[1] + ")," 
+                node (tabla.[nodo].[1]) (cad.Remove(0,1)) (st)
+            //  If it's not then it finishes with an E or error    
+            else
+                st <- state + "(q" + string nodo + ", " + result + ":a" + "->" + "E)" 
+                node (tabla.[nodo].[1]) (cad) (st) 
+        //  When the node is on q4 
+        elif (tabla.[nodo].[2] <> -1) then
+            //  If it is equal to b then it continues to the nex node
+            if result.Equals("b") then
+                st <- state + "(q" + string nodo + ", " + result + ":b" + "->" + "q" + string tabla.[nodo].[2] + ")," 
+                node (tabla.[nodo].[2]) (cad.Remove(0,1)) (st) 
+            //  If it's not then it finishes with an E or error   
+            else
+                st <- state + "(q" + string nodo + ", " + result + ":b" + "->" + "E)" 
+                node (tabla.[nodo].[2]) (cad) (st) 
+        //  When the node is on q1
+        elif (tabla.[nodo].[0] = -10) then
+            //  If it is equal to '' then it accepts the STRING
+            if result.Equals("''") then
+                st <- state + "(q" + string nodo + ", " + result + ":''" + "->" + "A)" 
+                node (tabla.[nodo].[0]) (cad) (st)  
+            //  If it's not then it finishes with an E or error  
+            else
+                st <- state + "(q" + string nodo + ", " + result + ":''" + "->" + "E)" 
+                node (tabla.[nodo].[0]) (cad) (st) 
+        else
+            //  If the node contains a lambda expression then it goes directly to the next nodes without comparing
+            st <- state + "(q" + string nodo + ", '':''->" + "q" + string tabla.[nodo].[0] + "),"
+            node (tabla.[nodo].[0]) (cad) (st)
+            //  If the node has a second state then it calls to another recursive node
+            if (tabla2.[nodo].[0] <> -1) then
+                st <- state + "(q" + string nodo + ", '':''->" + "q" + string tabla2.[nodo].[0] + "),"
+                node (tabla2.[nodo].[0]) (cad) (st)
+[<EntryPoint>]
+//  Main code
+let main argv =
+    //  Calls title to print the starting point
+    title ()
+    //  Asks for a string to be evaluated
+    let cadena = Console.ReadLine()
+    //  Calls the a recursive function node
+    node (0) (cadena) ("") 
+    0 // return an integer exit code
---
+++ AFN/AFN_170441/README.md
@@ -0,0 +1,49 @@
+#**COLLABORATOR**
+This code was made with the help of [Vazquez Reyes Rodolfo Emanuel - 171072]( https://github.com/upslp-teoriacomputacional/171072)
+# **How did you solve the problem?**
+To commence the coding of the NFA I modified the transition table of the a*ba* because the original one will cause an infinite loop and result in an error called “Stack Overflow”. Once that is changed, the next step was to divide the transition table into two tables just as it is displayed below.
+### Transition table 1
+| ESTADO  | ""  | a  | b  |
+| :------------: | :------------: | :------------: | :------------: |
+|Q0|Q2|E|E|
+|Q1|A|E|E|
+|Q2|Q3|E|E|
+|Q3|Q8|E|E|
+|Q4|E|E|Q5|
+|Q5|Q6|E|E|
+|Q6|Q7|E|E|
+|Q7|Q1|E|E|
+|Q8|E|Q9|E|
+|Q9|Q3|E|E|
+|Q10|E|Q11|E|
+|Q11|Q7|E|E|
+### Transition table 2
+| ESTADO  | ""  | a  | b  |
+| :------------: | :------------: | :------------: | :------------: |
+|Q0|E|E|E|
+|Q1|E|E|E|
+|Q2|Q8|E|E|
+|Q3|Q4|E|E|
+|Q4|E|E|E|
+|Q5|E|E|E|
+|Q6|Q10|E|E|
+|Q7|Q10|E|E|
+|Q8|E|E|E|
+|Q9|E|E|E|
+|Q10|E|E|E|
+|Q11|E|E|E|
+With this new transition table is possible to make a NFA that does not cycle infinitely. 
+The NFA program was made using recursive functions that receives the initial node, a string and a “”. With these parameters, the code makes as many branches as possible that will result in a error state or accept state of the initial string.
+# **Commands for executing operations**
+-   mutable The mutable keyword allows you to declare and assign values in a mutable variable.
+-   match       Pattern matching is used for control flow.
+-   Console.ReadLine()  Reads the next line of characters from the standard input stream.
+-   <>      Returns true if the left side is not equal to the right side; otherwise, returns false. 
+- try...with	Expression that is used for exception handling in the F# language.
+- String.Remove Method	Returns a new string in which a specified number of characters from the current string are deleted.
+- String.IndexOf Method	Reports the zero-based index of the first occurrence of a specified Unicode character or string within this instance. The method returns -1 if the character or string is not found in this instance.
+- String.Substring Method	Retrieves a substring from this instance. The substring starts at a specified character position and has a specified length.
+# **Problems and solutions during the work’s programming**
+This time, the main issue was the programming language itself. F# provides a good structure to build a comparison for the character, but in other aspects it will give you a hard time trying to find something more specific. For example, one of my problems was variables because I did no know that you could use mutable in order to alter the value assigned without having to declare it a second time.
+# **Is it a NP or P complexity?**
+Because the code includes a search, it indicates O(n²) for the complexity. Hence, the complexity will be polynomial.
