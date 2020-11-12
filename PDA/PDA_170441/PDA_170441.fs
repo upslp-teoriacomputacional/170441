0a31fcc902cd90a403e23684b78d92fa9fa8f14b
Gerardo Montalvo <gerardomontbec@hotmail.com>
2020-11-12 02:31:12 -0600

    PDA

--- PDA/PDA_170441/PDA_170441.fs
+++ PDA/PDA_170441/PDA_170441.fs
@@ -2,7 +2,6 @@
  * *****************************************************************************
  *         Name:    Juan Gerardo     
  *      Surname:    Montalvo Becerra
- * Collaborator:    Vazquez Reyes Rodolfo Emanuel
  *           ID:    170441
  *        Major:    IT Engineering
  *  Institution:    Universidad Politécnica de San Luis Potosí
@@ -10,13 +9,19 @@
  *  Description:    Made in VSC, using F# language.
  *
  *              
- *      Written:    15/10/2020
- * Last updated:    16/10/2020
+ *      Written:    12/11/2020
+ * Last updated:    13/11/2020
  **************************************************************************** *)
-open System 
+open System
+
 //  Because we have nodes with two states, we need a second table that has those second values
-let tabla = [ [2;-1;-1]; [-10;-1;-1]; [3;-1;-1]; [8;-1;-1]; [-1;-1;5]; [6;-1;-1]; [7;-1;-1]; [1;-1;-1]; [-1;9;-1]; [3;-1;-1]; [-1;11;-1]; [7;-1;-1] ]
-let tabla2 = [ [-1;-1;-1]; [-1;-1;-1]; [8;-1;-1]; [4;-1;-1]; [-1;-1;-1]; [-1;-1;-1]; [10;-1;-1]; [10;-1;-1]; [-1;-1;-1]; [-1;-1;-1]; [-1;-1;-1]; [-1;-1;-1] ]
+
+let tabla = [ [1;-1;-1]; [1;1;1]; [-10;-1;-1];]
+let tabla2 = [ [-1;-1;-1]; [2;-1;-1]; [-1;-1;-1];]
+let pdaRules = [["Z;SZ";"E";"E"];["S;e|Z;e";"S;SX";"X;e"];["E";"E";"E"];]
+let mutable nPrint = 1;
+
+
 //  Matching functions aba (Function Made by Vazquez Reyes Rodolfo Emanuel)
 let caracter (mtch:string) :string =
     //  match evaluates the first char of the string and returns the same value
@@ -102,7 +107,10 @@ let transitionTable (cd:string)(accepted:bool) =
     Console.ForegroundColor<-ConsoleColor.White
 //  Recursive function that display every posible way for the STRING in the NFA 
 //  (Function Made by Vazquez Reyes Rodolfo Emanuel & Montalvo Becerra Juan Gerardo)
-let rec node (nodo:int) (cad:string) (state:string) =
+
+let rec node (nodo:int) (cad:string) (state:string) (histStack:string) (stack:string) =
+    
+    let mutable stack = stack
     //  Variables that recover the current state and send the next
     let mutable st = ""
     let mutable proof = ""
@@ -113,13 +121,16 @@ let rec node (nodo:int) (cad:string) (state:string) =
     with
       | :? System.ArgumentOutOfRangeException -> printf ""
     //  At the start, it checks if it already acceptable
-    if (proof.Equals("A")) then
-        body ()
-        printfn "%A" state
-        Console.ForegroundColor<-ConsoleColor.Green
-        printfn " = It reaches the final state"
-        Console.ForegroundColor<-ConsoleColor.White
-        transitionTable state true
+    
+    if ( proof.Equals("A"))  then
+        if (stack.Length = 0) then
+            body ()
+            printfn "%A" state
+            Console.ForegroundColor<-ConsoleColor.Green
+            printfn " = It reaches the final state and the stack is empty"
+            Console.ForegroundColor<-ConsoleColor.White
+            transitionTable state true
+            histStack.Split '|' |> printfn "%A"
     //  It even checks if it's not acceptable
     elif (proof.Equals("E")) then
         body ()
@@ -128,58 +139,92 @@ let rec node (nodo:int) (cad:string) (state:string) =
         printfn " = It does not reach the final state"
         Console.ForegroundColor<-ConsoleColor.White
         transitionTable state false
+        histStack.Split '|' |> printfn "%A"
     //  If neither of those occurred, it continues until it occurs    
     else
+        let histStack = histStack + "|" + stack
         let mutable chain = ""
         //  Uses the function match on the cad string
         let result = caracter cad
+        if (tabla.[nodo].[1] <> -1 || tabla.[nodo].[2] <> -1 || tabla.[nodo].[0] = -10) then
         //  When the node is on q8 or q10
-        if (tabla.[nodo].[1] <> -1) then
-            //  If it is equal to a then it continues to the nex node
-            if result.Equals("a") then
-                st <- state + "(q" + string nodo + ", " + result + ":a" + "->" + "q" + string tabla.[nodo].[1] + ")," 
-                node (tabla.[nodo].[1]) (cad.Remove(0,1)) (st)
-            //  If it's not then it finishes with an E or error    
-            else
-                st <- state + "(q" + string nodo + ", " + result + ":a" + "->" + "E)" 
-                node (tabla.[nodo].[1]) (cad) (st) 
-        //  When the node is on q4 
-        elif (tabla.[nodo].[2] <> -1) then
-            //  If it is equal to b then it continues to the nex node
-            if result.Equals("b") then
-                st <- state + "(q" + string nodo + ", " + result + ":b" + "->" + "q" + string tabla.[nodo].[2] + ")," 
-                node (tabla.[nodo].[2]) (cad.Remove(0,1)) (st) 
-            //  If it's not then it finishes with an E or error   
-            else
-                st <- state + "(q" + string nodo + ", " + result + ":b" + "->" + "E)" 
-                node (tabla.[nodo].[2]) (cad) (st) 
-        //  When the node is on q1
-        elif (tabla.[nodo].[0] = -10) then
-            //  If it is equal to '' then it accepts the STRING
-            if result.Equals("''") then
-                st <- state + "(q" + string nodo + ", " + result + ":''" + "->" + "A)" 
-                node (tabla.[nodo].[0]) (cad) (st)  
-            //  If it's not then it finishes with an E or error  
-            else
-                st <- state + "(q" + string nodo + ", " + result + ":''" + "->" + "E)" 
-                node (tabla.[nodo].[0]) (cad) (st) 
-        else
+            if (tabla.[nodo].[1] <> -1) then
+                //  If it is equal to a then it continues to the nex node
+                if result.Equals("a") then
+                    for rule in pdaRules.[nodo].[1].Split '|' do
+                        try
+                            if ((stack.Substring(0,1)) = (rule.Substring(0,1)) ) then
+                                stack <- stack.Substring(1)
+                                stack <- rule.Substring(rule.IndexOf(";") + 1) + stack
+                                st <- state + "(q" + string nodo + ", " + result + ":a" + "->" + "q" + string tabla.[nodo].[1] + ")," 
+                                node (tabla.[nodo].[1]) (cad.Remove(0,1)) (st) (histStack) (stack)
+                        with
+                           | :? System.ArgumentOutOfRangeException -> () 
+                //  If it's not then it finishes with an E or error    
+                else
+                    st <- state + "(q" + string nodo + ", " + result + ":a" + "->" + "E)" 
+                    node (tabla.[nodo].[1]) (cad) (st) (histStack) (stack)
+            //  When the node is on q4 
+            if (tabla.[nodo].[2] <> -1) then
+                //  If it is equal to b then it continues to the nex node
+                if result.Equals("b") then
+                    for rule in pdaRules.[nodo].[2].Split '|' do
+                        try
+                            if ((stack.Substring(0,1)) = (rule.Substring(0,1)) ) then
+                                if (rule.Substring(rule.Length-1, 1).Equals("e")) then
+                                    stack <- stack.Substring(1)
+                                st <- state + "(q" + string nodo + ", " + result + ":b" + "->" + "q" + string tabla.[nodo].[2] + ")," 
+                                node (tabla.[nodo].[2]) (cad.Remove(0,1)) (st) (histStack) (stack)
+                        with
+                           | :? System.ArgumentOutOfRangeException -> ()
+                        
+                //  If it's not then it finishes with an E or error   
+                else
+                    st <- state + "(q" + string nodo + ", " + result + ":b" + "->" + "E)" 
+                    node (tabla.[nodo].[2]) (cad) (st) (histStack) (stack)
+            //  When the node is on q1
+            if (tabla.[nodo].[0] = -10) then
+                //  If it is equal to '' then it accepts the STRING
+                if result.Equals("''") then
+                    st <- state + "(q" + string nodo + ", " + result + ":''" + "->" + "A)" 
+                    node (tabla.[nodo].[0]) (cad) (st) (histStack) (stack)  
+                //  If it's not then it finishes with an E or error  
+                else
+                    st <- state + "(q" + string nodo + ", " + result + ":''" + "->" + "E)" 
+                    node (tabla.[nodo].[0]) (cad) (st) (histStack) (stack)
+
+        if (tabla.[nodo].[0] <> -1 && tabla.[nodo].[0] <> -10) then
+            
             //  If the node contains a lambda expression then it goes directly to the next nodes without comparing
-            st <- state + "(q" + string nodo + ", '':''->" + "q" + string tabla.[nodo].[0] + "),"
-            node (tabla.[nodo].[0]) (cad) (st)
+            
+            for rule in pdaRules.[nodo].[0].Split '|' do
+                try
+                    if ((stack.Substring(0,1)) = (rule.Substring(0,1)) ) then
+                        if (rule.Substring(rule.Length-1).Equals("e")) then
+                            stack <- stack.Substring(1)
+                        else
+                            stack <- stack.Substring(1)
+                            stack <- rule.Substring(rule.IndexOf(";") + 1) + stack
+                        st <- state + "(q" + string nodo + ", '':''->" + "q" + string tabla.[nodo].[0] + "),"
+                        if (tabla2.[nodo].[0] <> -1) then
+                            st <- state + "(q" + string nodo + ", '':''->" + "q" + string tabla2.[nodo].[0] + "),"
+                            node (tabla2.[nodo].[0]) (cad) (st) (histStack) (stack)
+                        node (tabla.[nodo].[0]) (cad) (st) (histStack) (stack)
+                with
+                    | :? System.ArgumentOutOfRangeException -> ()
             //  If the node has a second state then it calls to another recursive node
-            if (tabla2.[nodo].[0] <> -1) then
-                st <- state + "(q" + string nodo + ", '':''->" + "q" + string tabla2.[nodo].[0] + "),"
-                node (tabla2.[nodo].[0]) (cad) (st)
+            
+            
+
 [<EntryPoint>]
+
 //  Main code
 let main argv =
-    let a =""
-    a.Substring(0,1) |> printfn "%A"
     //  Calls title to print the starting point
     title ()
     //  Asks for a string to be evaluated
     let cadena = Console.ReadLine()
     //  Calls the a recursive function node
-    node (0) (cadena) ("") 
+
+    node (0) (cadena) ("") ("") ("Z")
     0 // return an integer exit code
